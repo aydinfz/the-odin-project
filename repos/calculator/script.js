0e8a7907f764b7de26d24e8f270955aa9ab49575
Zehra <aydin.fzehra@gmail.com>
2022-08-04 22:46:22 +0300

    update

--- repos/calculator/script.js
+++ repos/calculator/script.js
@@ -28,3 +28,31 @@ function operate (num1, operator, num2){
 }
 
 console.log(operate(3, "*", 5))
+
+let bodyButtons = document.querySelectorAll(".body-buttons button")
+bodyButtons.forEach(button => button.addEventListener("click", showText))
+
+
+let screen = document.querySelector(".screen")
+let operation = []
+let firstNum;
+let secondNum;
+let displayedText;
+let index;
+function showText(e) {
+    operation += e.target.innerText
+    screen.innerText = operation 
+    let displayedText = screen.innerText
+    console.log(getIndexOfOperator(displayedText))
+
+}
+
+
+function getIndexOfOperator (text) {
+    let indexOfOp = (/\+|-|x|÷/gm).exec(text)
+    if (indexOfOp != null) {
+        index = indexOfOp.index
+    }
+    return index     
+}
+
