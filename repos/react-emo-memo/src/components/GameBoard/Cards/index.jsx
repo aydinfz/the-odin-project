8042a4ec2997fa11fe655a60f34001b5a3594ddf
Zehra <aydin.fzehra@gmail.com>
2022-11-19 17:04:27 +0300

    update functions

--- repos/react-emo-memo/src/components/GameBoard/Cards/index.jsx
+++ repos/react-emo-memo/src/components/GameBoard/Cards/index.jsx
@@ -6,6 +6,11 @@ import { Card } from "../Card";
 export function Cards() {
   const BASE_URL = "https://emoji-api.com/emojis";
   const [data, setData] = useState([]);
+  const [lvl, setLvl] = useState(1);
+  const [cardsNum, setCardsNum] = useState(3);
+  
+  const ref = useRef([]);
+  const selectedCards = ref.current;
 
   useEffect(() => {
     async function fetchEmojis() {
@@ -13,31 +18,42 @@ export function Cards() {
         `${BASE_URL}?access_key=${import.meta.env.VITE_API_KEY}`
       );
       const json = await response.json();
-      const result = await json.slice(0, 13);
+      const result = await json.slice(0, cardsNum);
       setData(result);
     }
     fetchEmojis();
-  }, []);
+  }, [cardsNum]);
 
-  const ref = useRef([]);
   const shuffle = (_data) => {
     return [..._data].sort(() => 0.5 - Math.random());
   };
 
   const addCardId = (id) => {
     ref.current = [id, ...ref.current];
-    checkCardsId(ref.current);
+    checkCardsId();
     setData(shuffle(data));
   };
 
-  const checkCardsId = (_cardsId) => {
-    isGameOver(new Set(_cardsId).size !== _cardsId.length);
+  const checkCardsId = () => {
+    return new Set(selectedCards).size === selectedCards.length;
   };
 
-  const isGameOver = (round) => {
-    if (round) console.log("dieee");
-    else console.log("continuee");
+  useEffect(() => {
+    ref.current = [];
+    nextLvl();
+  }, [lvl]);
+
+  const nextLvl = () => {
+    if (cardsNum === selectedCards.length && checkCardsId() === true) {
+      setLvl(lvl + 1);
+      setCardsNum(2 * lvl + 2);
+    }
   };
+  nextLvl();
+
+  console.log({ lvl: lvl });
+  console.log(selectedCards);
+  console.log(checkCardsId());
 
   return (
     <Wrapper>
